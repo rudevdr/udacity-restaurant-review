cdec5ad27441826464907041eb3fd13c514153a0
rudevdr <gudayschief@gmail.com>
2018-11-22 05:12:00 +0530

    working Service Worker implementation

---
+++ js/RegisterServiceWorker.js
@@ -0,0 +1,13 @@
+//Registers Service Worker in index.html
+
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', function() {
+		navigator.serviceWorker.register('/ServiceWorker.js').then(function(registration) {
+			// Registration was successful
+			console.log('ServiceWorker registration successful with scope: '+ registration.scope);
+		}, function(err) {
+			// registration failed :(
+			console.log('ServiceWorker registration failed: '+ err);
+		});
+	});
+}
